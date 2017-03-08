aaf36ac41330df3eb02dc6077b8c5956209f3cfc
chtison <ch4rles.contact@gmail.com>
2017-03-08 21:45:49 +0100

    .

--- convert.py
+++ convert.py
@@ -10,3 +10,10 @@ def rgb_to_uicolor(color, alpha=1, precision=2, prefix='UI'):
 	g = round(((color & 0x00FF00) >>  8) / 255.0, 2)
 	b = round(((color & 0x0000FF) >>  0) / 255.0, 2)
 	return '{prefix}Color(red:{:.{prec}f}, green:{:.{prec}f}, blue:{:.{prec}f}, alpha:{alpha})'.format(r, g, b, alpha=alpha, prec=precision, prefix=prefix)
+
+if __name__ == '__main__':
+	from cmds import main
+	cmds = [
+		'rgb_to_uicolor',
+	]
+	main('convert', cmds)
